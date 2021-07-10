465545b519de8c93581d90016bce922af53994b6
SymphonySimper <SymphonySimper@users.noreply.github.com>
2021-07-10 15:09:57 +0530

    pywal boii

--- config.def.h
+++ config.def.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
+#include "/home/s2b/.cache/wal/colors-wal-dmenu.h"
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -7,13 +8,6 @@ static const char *fonts[] = {
 	"poppins:size=10:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#111111" },
-	[SchemeSel] = { "#aaaaaa", "#444444" },
-	[SchemeOut] = { "#aaaaaa", "#111111" },
-	[SchemeHp] = { "#aaaaaa", "#111111" }
-};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
